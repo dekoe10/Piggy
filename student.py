47b2820999feccaf5cd631f39c0e2150ec38e9e9
Dylan Dekoe <dekoed@gilmour.org>
2019-12-18 08:58:56 -0500

    MOTOR RIGHT

--- student.py
+++ student.py
@@ -226,7 +226,7 @@ class Piggy(PiggyParent):
                 left_speed -= 10
                 right_speed += 5
             self.set_motor_power(self.MOTOR_LEFT, left_speed)
-            self.set_motor_power(self.MOTOR_right, right_speed)
+            self.set_motor_power(self.MOTOR_RIGHT, right_speed)
             time.sleep(.1)  
 
     
